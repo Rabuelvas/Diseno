d49fc1f20beff9b31643c0f5f0070538de5754f7
Rabuelvas <rabuelvas@uninorte.edu.co>
2022-04-26 08:52:36 -0500

    Envío a instancias

--- app/src/main/java/com/example/tcp/MainActivity.java
+++ app/src/main/java/com/example/tcp/MainActivity.java
@@ -3,36 +3,46 @@ package com.example.tcp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.LocationRequest;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Locale;
 
-import android.util.Log;
 import android.widget.Switch;
 
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -45,12 +55,58 @@ public class MainActivity extends AppCompatActivity {
     private EditText etIP, port, Mensaje;
     private Button Localizar, UDP;
     private String mess;
-    private Switch Switch;
-    private Handler loop = new Handler();
+    private Switch Switch, Switch2;
+
     FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    private final Handler loop = new Handler();
     Socket s;
     PrintWriter pw;
     DatagramSocket udpSocket;
+
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            if (locationResult == null) {
+                return;
+            }
+            for (Location location : locationResult.getLocations()) {
+                try {
+                    //Initialize  geoCoder
+                    Geocoder geocoder = new Geocoder(MainActivity.this,
+                            Locale.getDefault());
+                    //Initialize address list
+
+                    List<Address> addresses = geocoder.getFromLocation(
+                            location.getLatitude(), location.getLongitude(), 1
+                    );
+
+                    //Set latitude en texto
+
+
+                    Location loc = location;
+                    double lat = loc.getLatitude();
+                    double log = loc.getLongitude();
+
+                    String latitud = String.valueOf(lat);
+                    String longitud = String.valueOf(log);
+
+                    Calendar c = Calendar.getInstance();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String strDate = sdf.format(c.getTime());
+
+                    Mensaje.setText("Latitud: " + latitud + "\nLongitud: " + longitud + "\nTimeStamp :" + strDate);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private static final String TAG = "MainActivity";
+    int LOCATION_REQUEST_CODE = 10001;
 
 
     @Override
@@ -59,13 +115,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         Mensaje = findViewById(R.id.ubicacion);
-        Localizar = findViewById(R.id.bt_location);
+        Localizar = findViewById(R.id.coordenadas);
         etIP = findViewById(R.id.etIP);
         port = findViewById(R.id.etPuerto);
         UDP = findViewById(R.id.udp);
         Switch = findViewById(R.id.switchE);
+        Switch2 = findViewById(R.id.switchIns);
 
         Localizar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,72 +173,203 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
-    public void onclick(View view) {
 
-        if(view.getId()==R.id.switchE) {
-            if(Switch.isChecked()) {
-                ubicacion.run();
-                enviar.run();
-            }else {
-                loop.removeCallbacks(ubicacion);
-                loop.removeCallbacks(enviar);
+
+    public void start() {
+        LocationSettingsRequest request = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build();
+
+        SettingsClient client = LocationServices.getSettingsClient(this
+        );
+
+        Task<LocationSettingsResponse> locationSettingsResponseTask = client.checkLocationSettings(request);
+        locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // start location update
+                qwerty();
+
+            }
+        });
+        locationSettingsResponseTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    ResolvableApiException apiException = (ResolvableApiException) e;
+                    try {
+                        apiException.startResolutionForResult(MainActivity.this, 1001);
+                    } catch (IntentSender.SendIntentException sendIntentException) {
+                        sendIntentException.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+
+    }
+
+    public void qwerty() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
+    public void enable() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                Log.d(TAG, "asklocationPermission: Alert ");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            } else {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+
             }
         }
     }
-        private Runnable ubicacion = new Runnable() {
 
-            @Override
-            public void run() {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso Garantizado
 
-                    getlocation();
+                start();
+            } else {
+                // Permiso Negado
+                qwerty();
+            }
+        }
+    }
+
+    public void onclick(View view) {
+
+        if (view.getId() == R.id.switchE) {
+            if (Switch.isChecked()) {
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+
+                    start();
+                    enviar.run();
+
                 } else {
-
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]
-                            {Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                    enable();
                 }
-                loop.postDelayed(this,5000);
+            } else {
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+                loop.removeCallbacks(enviar);
             }
-        };
+        }
 
+        if (view.getId() == R.id.switchIns) {
+            if (Switch2.isChecked()) {
 
-        private Runnable enviar = new Runnable() {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
 
-            @Override
-            public void run(){;
-                int puerto = Integer.parseInt(port.getText().toString());
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mess = Mensaje.getText().toString();
-                        try {
-                            udpSocket = new DatagramSocket(puerto);
-                            InetAddress serverAddr = InetAddress.getByName(etIP.getText().toString());
-                            byte[] buf = (mess).getBytes();
-                            DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddr, puerto);
-                            udpSocket.send(packet);
-                            udpSocket.close();
-                        } catch (SocketException e) {
-                            //Log.e("Udp:", "Socket Error:", e);
-                        } catch (IOException e) {
-                            //Log.e("Udp Send:", "IO Error:", e);
-                        }
+                    start();
+                    enviarToIns.run();
 
+                } else {
+                    enable();
+                }
+            } else {
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+                loop.removeCallbacks(enviarToIns);
+            }
+        }
+    }
+
+    private Runnable enviar = new Runnable() {
+
+        @Override
+        public void run() {
+
+            int puerto = Integer.parseInt(port.getText().toString());
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    mess = Mensaje.getText().toString();
+                    try {
+                        udpSocket = new DatagramSocket(puerto);
+                        InetAddress serverAddr = InetAddress.getByName(etIP.getText().toString());
+                        byte[] buf = (mess).getBytes();
+                        DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddr, puerto);
+                        udpSocket.send(packet);
+                        udpSocket.close();
+                    } catch (SocketException e) {
+                        //Log.e("Udp:", "Socket Error:", e);
+                    } catch (IOException e) {
+                        //Log.e("Udp Send:", "IO Error:", e);
                     }
+
                 }
-                ).start();
-                loop.postDelayed(this,5000);
             }
+            ).start();
+            loop.postDelayed(this, 5000);
+        }
 
 
-        };
+    };
+
+    private Runnable enviarToIns = new Runnable() {
+
+        @Override
+        public void run() {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    mess = Mensaje.getText().toString();
+                    try {
+                        udpSocket = new DatagramSocket(3020);
+                        InetAddress serverInst = InetAddress.getByName("3.213.123.18");
+                        InetAddress serverInst2 = InetAddress.getByName("34.239.66.120");
+                        InetAddress serverInst3 = InetAddress.getByName("44.198.119.172");
+                        InetAddress serverInst4 = InetAddress.getByName("3.225.130.220");
+                        InetAddress serverInst5 = InetAddress.getByName("10.20.30.47");
+
+                        byte[] buf = (mess).getBytes();
+                        DatagramPacket packet = new DatagramPacket(buf, buf.length, serverInst, 3020);
+                        DatagramPacket packet2 = new DatagramPacket(buf, buf.length, serverInst2, 3020);
+                        DatagramPacket packet3 = new DatagramPacket(buf, buf.length, serverInst3, 3020);
+                        DatagramPacket packet4 = new DatagramPacket(buf, buf.length, serverInst4, 3020);
+                        DatagramPacket packet5 = new DatagramPacket(buf, buf.length, serverInst5,3020);
+
+                        udpSocket.send(packet);
+                        udpSocket.send(packet2);
+                        udpSocket.send(packet3);
+                        udpSocket.send(packet4);
+                        udpSocket.send(packet5);
+
+                        udpSocket.close();
+                    } catch (SocketException e) {
+                        //Log.e("Udp:", "Socket Error:", e);
+                    } catch (IOException e) {
+                        //Log.e("Udp Send:", "IO Error:", e);
+                    }
+
+                }
+            }
+            ).start();
+            loop.postDelayed(this, 5000);
+        }
 
 
-
-
-
-
+    };
 
     private void getlocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
@@ -217,9 +409,7 @@ public class MainActivity extends AppCompatActivity {
                         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                         String strDate = sdf.format(c.getTime());
 
-                        String dir = String.valueOf(addresses.get(0).getAddressLine(0));
-
-                        Mensaje.setText("Latitud: " + latitud + "\nLongitud: " + longitud+"\nTimeStamp :"+strDate);
+                        Mensaje.setText("Latitud: " + latitud + "\nLongitud: " + longitud + "\nTimeStamp :" + strDate);
 
 
                     } catch (IOException e) {
@@ -233,6 +423,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
 }
